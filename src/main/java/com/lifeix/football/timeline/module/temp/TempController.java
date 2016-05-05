85858ff99391f7e708dc5a24280f2d9eb67f3860
venizeng <venizeng@gmail.com>
2016-05-05 19:35:59 +0800

    temp

--- src/main/java/com/lifeix/football/timeline/module/temp/TempController.java
+++ src/main/java/com/lifeix/football/timeline/module/temp/TempController.java
@@ -11,7 +11,7 @@ public class TempController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String print(){
-        return "hello";
+        return "hello1";
     }
 
 }
